fc5038bd23597c86344aa96c745cf1355ce03e12
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-09-08 12:54:33 +0200

    feat: random participants + random name

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 
 function App() {
 
-  useEffect(() =>{
-    
+  const [participants, setParticipants] = useState(null);
+  const [fighters, setFighters]= useState([])
 
+  useEffect(() =>{
+    NumParticipants();
+    setFighters(generateName())
+    console.log(fighters);
+    
   },[]);
 
+  //genera un numero casuale di partecipanti
+  function NumParticipants(){
+    setParticipants(Math.floor(Math.random() * 50) + 1);
+  }
   
+  //andiamo a generare dei nomi casuali in base al numero di partecipanti
+  function generateName(participants){
+    return Array.from({ length:  participants}, () => faker.person.firstName());
+  } 
+
+  
+
 
   return (
     <>
       <h2 className='text-center '>ciao</h2>
-      <div></div>
+      {/* <ul>
+        {fighters.map((name, id) =>(
+          <li key={id}>{name}</li>
+        ))} 
+      </ul>*/}
     </>
   )
 }
