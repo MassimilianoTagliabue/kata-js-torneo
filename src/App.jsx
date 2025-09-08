d603e9a25d78ce64bf61f9736b6895243a9df135
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-09-08 12:05:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+
+function App() {
+
+  useEffect(() =>{
+    
+
+  },[]);
+
+  
+
+  return (
+    <>
+      <h2 className='text-center '>ciao</h2>
+      <div></div>
+    </>
+  )
+}
+
+export default App
