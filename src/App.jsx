e5dfb468975d30ceb261fdab1cab0f484846102d
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-09-11 15:55:34 +0200

    feat: logo + title

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,23 @@ function App() {
 
   }, []);
 
+
+
   //genera un numero casuale
   function randomNum() {
     return Math.floor(Math.random() * 100);
   }
 
+  
+  const arraynum = [];
   //controlla che non venga scelto 2 volte lo stesso numero
   function duplicate(num) {
 
     if (arraynum.includes(num)) { //controllo se num è presente nell'array
       const arraynumLength = arraynum.length; //salvo la lunghezza di arraynum
       
-
       while (arraynumLength === arraynum.length) { //il ciclo continua fino a quando non viene aggiunto un nuovo elemento
-        
-
+      
         const num2 = randomNum(); //genero un nuovo numero
         if (!arraynum.includes(num2)) { // controllo se non è presente
           arraynum.push(num2);  //aggiungo il numero
@@ -35,21 +37,18 @@ function App() {
       }
 
     } else {//se non è presente lo aggiungo
-      
       arraynum.push(num);
-      
-
     }
-
   }
 
 
-  const arraynum = [];
+
+
+  
 
   fighters.forEach((curItem) => {
 
     const num = randomNum();
-
     duplicate(num);
 
 
