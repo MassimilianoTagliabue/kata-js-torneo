150300fd5aad8d11cbfe302b93caa322bf4a4b5e
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-09-19 15:19:17 +0200

    feat: function tournament

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,70 @@ function App() {
 
   useEffect(() => {
     setArmedFighters(generatedArmedFighters());
-  
+
   }, []);
 
 
   //fase 2: training
-  function handleTraining(){
+  function handleTraining() {
     setArmedFighters(training(armedFighters));
 
     //vado a disattivare il bottone 
     setDisabled(true);
   }
-                
+
 
   //fase 3: qualification
-  function handleQualification(){
+  function handleQualification() {
     setArmedFighters(qualification(armedFighters))
   }
 
 
+  //fase 4: combattimento
+
+  function Fight(player1, player2) {
+    if (player1.power > player2.power) {
+      return player1
+    } else if (player2.power > player1.power) {
+      return player2
+    } else {
+      return player1
+    }
+  }
+
+
+  function playRound(armedFighters) {
+    let winners = []
+    for(let i=0; i<armedFighters.length; i+= 2){
+      
+      winners.push(Fight(armedFighters[i], armedFighters[i + 1]));
+    }
+    return winners
+  }
+
+
+  function Tournament(armedFighters) {
+    let round = 1;
+    while(armedFighters.length > 1){
+      console.log("round N° " + round + armedFighters);
+      setArmedFighters(playRound(armedFighters));
+      round++;
+    }
+    //console.log("Vincitore " + armedFighters);
+    
+  }
+
+  function startTournament(){
+    Tournament(armedFighters)
+  }
 
   return (
     <>
       <h2 className='text-center '>ciao</h2>
-      
+
       <button className="btn btn-primary mx-5 my-5" onClick={handleTraining} disabled={disabled}> allenamento</button>
       <button className="btn btn-primary mx-5 my-5" onClick={handleQualification}>qualifica</button>
+      <button className="btn btn-success mx-5 my-5" onClick={startTournament}>inizia Torneo</button>
     </>
   )
 }
