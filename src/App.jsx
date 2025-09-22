fadd6478b91c9e35dd676d128e99b4acd59a682e
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-09-22 12:19:28 +0200

    update: fix tuornament fuction + comment

--- src/App.jsx
+++ src/App.jsx
@@ -36,38 +36,86 @@ function App() {
   //fase 4: combattimento
 
   function Fight(player1, player2) {
-    if (player1.power > player2.power) {
-      return player1
-    } else if (player2.power > player1.power) {
-      return player2
-    } else {
-      return player1
-    }
+    //operatore ternario che controlla il vincitore
+    return (player1 >= player2) ? player1 : player2;
   }
 
 
-  function playRound(armedFighters) {
+  // gioca un round: restituisce sia i vincitori che i perdenti
+  function playRound(players) {
     let winners = []
-    for(let i=0; i<armedFighters.length; i+= 2){
-      
-      winners.push(Fight(armedFighters[i], armedFighters[i + 1]));
+    let losers = []
+    
+
+    for (let i = 0; i < players.length; i += 2) {
+      const p1 = players[i];
+      const p2 = players[i + 1];
+
+      const winner = Fight(p1, p2);
+
+      winners.push(winner);
+      losers.push(winner === p1 ? p2 : p1);
     }
-    return winners
+
+    return { winners, losers }
   }
 
-
-  function Tournament(armedFighters) {
+  //struttura del torneo
+  function Tournament(fighters) {
+    console.log(fighters);
+    let players = [...fighters];
+    let semiFinalLosers = [];
     let round = 1;
-    while(armedFighters.length > 1){
-      console.log("round N° " + round + armedFighters);
-      setArmedFighters(playRound(armedFighters));
+    let second;
+
+    while (players.length > 1) {
+
+      console.log("round N° " + round);
+      const { winners, losers } = playRound(players);
+
+
+      // controllo di sicurezza: se playRound non riduce il numero, evito loop infinito
+      if (winners.length === players.length) {
+        console.error("Errore: playRound non ha ridotto il numero di giocatori. Interrompo il torneo per evitare loop infinito.");
+        break;
+      }
+
+      // se sto giocando le semifinali, i 'losers' faranno lo spareggio per il 3° posto
+      if (players.length === 4) {
+        semiFinalLosers = [...losers];
+      }
+
+      if(players.length === 2){
+        second = losers[0];
+      }
+
+      //aggiorno players per evitare un loop infinito
+      players = winners;
       round++;
     }
-    //console.log("Vincitore " + armedFighters);
+
     
+    //stabilisco il campione
+    const champion = players[0];
+    let third;
+    
+    
+    //spareggio per il 3° posto
+    if(semiFinalLosers === 2){
+      third = Fight(semiFinalLosers[0], semiFinalLosers[1]);
+      console.log(`🥉 Terzo posto: ${third.name}`);
+    }
+
+    console.log("Vicitore = " + champion.name);
+    console.log("Secondo Posto = " + second.name);
+    //console.log("Secondo Posto = " + second.name);
+
+
   }
 
-  function startTournament(){
+
+
+  function startTournament() {
     Tournament(armedFighters)
   }
 
