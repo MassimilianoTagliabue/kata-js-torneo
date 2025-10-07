835986dfd0b19c289944859869f2176384ce1efa
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-10-07 15:21:02 +0200

    feat: confetti animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import generatedArmedFighters from "./utils/GenerateArmedFighters";
 import training from "./utils/Training";
 import qualification from "./utils/Qualification";
 import Tournament from "./utils/Tournament";
 import { GlobalContext } from "./context/GlobalContext";
-import FighterCard from "./components/FighterCard"
+import FighterCard from "./components/FighterCard";
+import Confetti from "react-confetti";
+
 
 const turnBtn = {
   btnTraining: false,
   btnQualification: true,
-  btnStart:true
+  btnStart: true
 }
 
 
@@ -19,7 +21,9 @@ function App() {
   const [turnOff, setTurnOff] = useState(turnBtn);  //serve a disattivare il bottone dopo il click
   const [showWinner, setShowWinner] = useState(false);
   const [startedTournament, setStartedTournament] = useState(false);
-  const [winners, setWinners] = useState([])
+  const [winners, setWinners] = useState([]);
+  const [showConfetti, setShowConfetti] = useState(false);
+
 
   useEffect(() => {
     setArmedFighters(generatedArmedFighters());
@@ -28,8 +32,8 @@ function App() {
 
   useEffect(() => {
     console.log(winners);
-    
-  },[winners])
+
+  }, [winners])
 
 
   //fase 2: training
@@ -39,8 +43,8 @@ function App() {
     //vado a disattivare il bottone 
     setTurnOff(btn => ({
       ...btn,
-      btnTraining:true,
-      btnQualification:false
+      btnTraining: true,
+      btnQualification: false
     }));
 
   }
@@ -51,31 +55,31 @@ function App() {
     setArmedFighters(qualification(armedFighters))
     setTurnOff(btn => ({
       ...btn,
-      btnStart:false,
-      btnQualification:true
+      btnStart: false,
+      btnQualification: true
     }));
   }
 
 
   //fase 4: tournament
-  
+
   function startTournament() {
-    const [champion,second,third] = Tournament(armedFighters);
+    const [champion, second, third] = Tournament(armedFighters);
 
     const tempWinner = [champion, second, third];
     //console.log(tempWinner);
-    
-      setWinners(tempWinner)
 
-    
-    
-    
+    setWinners(tempWinner)
+
+
+
+
     setStartedTournament(true)
     setTurnOff(btn => ({
       ...btn,
-      btnStart:true
+      btnStart: true
     }));
-// 
+    // 
   }
 
 
@@ -94,7 +98,7 @@ function App() {
     <GlobalContext.Provider value={globalProviderValue}>
       <h2 className='text-center my-4'>Benvenuto al Torne Boolkaichi</h2>
 
-      
+
       <div className="container " onChange={setArmedFighters}>
 
         <button className="btn btn-primary mx-5 my-5" onClick={handleTraining} disabled={turnOff.btnTraining}> Allenamento</button>
@@ -104,7 +108,14 @@ function App() {
         {/* tasto visibile solo se il torneo è iniziato */}
         {startedTournament ? (
           <>
-            <button className="btn btn-warning mx-5 my-5" onClick={() => { showWinner ? (setShowWinner(false)) : (setShowWinner(true)) }}>
+            <button className="btn btn-warning mx-5 my-5" onClick={() => {
+              showWinner ? (setShowWinner(false)) : (setShowWinner(true));
+
+              if(!showWinner){
+                setShowConfetti(true);
+                setTimeout(() => setShowConfetti(false), 5000);
+              }
+            }}>
               {showWinner ? ("Nascondi Classifica") : ("Mostra Classifica")}</button>
           </>
         ) : (
@@ -115,9 +126,21 @@ function App() {
           </>
         )}
 
+        {/* 🎉 Confetti (si attiva solo quando showConfetti è true) */}
+        {showConfetti && <Confetti recycle={false} numberOfPieces={300} />}
 
         {showWinner ? (
           <>
+            {winners.map((winner, index) => {
+              return (
+                <div key={index} className="mb-5 col-3">
+
+
+                  <FighterCard
+                    fighter={winner}
+                  />
+                </div>)
+            })}
           </>
 
         ) : (
